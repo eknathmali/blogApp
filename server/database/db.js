f057b80c199a8f40a52d021fbbb1da0418199708
Eknath Mali <89828172+eknathmali@users.noreply.github.com>
2023-09-13 21:26:41 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -3,10 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 const USERNAME = process.env.DB_USER
 const PASSWORD = process.env.DB_PASS
-
+const pass = "Eknath@=7249"
 // const Connection = async(URL) =>{
 const Connection = async() =>{
-    const url = `mongodb+srv://${USERNAME}:` + encodeURIComponent(PASSWORD) + "@cluster0.bwsjzaf.mongodb.net/dbs";
+    // const url = `mongodb+srv://${USERNAME}:` + encodeURIComponent(PASSWORD) + "@cluster0.bwsjzaf.mongodb.net/dbs";
+    const url  = `mongodb+srv://dm4514533:${pass}@cluster0.bwsjzaf.mongodb.net/Cluster0?retryWrites=true&w=majority`;
     // const url ="mongodb://127.0.0.1:27017/blog";
     try{
        await mongoose.connect(URL , {useNewUrlParser : true});
